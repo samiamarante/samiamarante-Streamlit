7293b7352438fe8bb8cd66868549e67a49df01cb
ffer <ffer200395@gmail.com>
2023-11-24 14:55:02 +0100

    Pred con Streamlit

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -1,24 +1,60 @@
 import streamlit as st
+import pandas as pd
+import pickle
 # Local: streamlit run streamlit_tutorial.py
 # Streamlit Sharing 
 # render, heroku, AWS EC2
-
+# ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)','petal width (cm)']
 def main():
-    st.title('Contador Simple')
+    option = st.radio('Selecciona una opción:', ('Ingresar Datos', 'Usar Datos del CSV'))
+
+    if option == 'Ingresar Datos':
+        st.write('**Ingresa los datos para realizar la predicción:**')
+        
+        # Aquí puedes solicitar los datos para la predicción
+        # Por ejemplo, si estás prediciendo con un modelo de regresión lineal:
+        input_data = {}
+        input_data['sepal length (cm)'] = st.number_input('sepal length (cm)', value=0.0)
+        input_data['sepal width (cm)'] = st.number_input('sepal width (cm)', value=0.0)
+        input_data['petal length (cm)'] = st.number_input('petal length (cm)', value=0.0)
+        input_data['petal width (cm)'] = st.number_input('petal width (cm)', value=0.0)
+        # Continúa con todas las características necesarias para la predicción
+        
+        if st.button('Realizar Predicción'):
+            # Realiza la predicción con los datos ingresados
+            predicted_value = make_prediction(pd.DataFrame(input_data))
+            st.write('El resultado de la predicción es:', predicted_value)
+
+    elif option == 'Usar Datos del CSV':
+        st.title('Predicción con Archivo CSV')
+        # Widget para subir archivos CSV
+        uploaded_file = st.file_uploader("Selecciona un archivo CSV", type=['csv'])
+        if uploaded_file is not None:              
+            # Lee el archivo CSV como un DataFrame
+            df = pd.read_csv(uploaded_file)
+            st.write('**Vista Previa del DataFrame:**')
+            # Muestra el DataFrame
+            st.write(df)
+            st.subheader('Realizar Predicción')
+            st.write('**Selecciona las columnas para la predicción:**')
+            # Aquí el usuario elige qué columnas usar para la predicción
+            # Por ejemplo, si se tienen columnas 'Feature_1', 'Feature_2', 'Target':
+            feature_cols = st.multiselect('Selecciona las características', df.columns)
+        if st.button('Realizar Predicción con CSV'):
+            # Realiza la predicción con los datos del CSV seleccionados
+            predicted_values = make_prediction(df[feature_cols])
+            st.write('Los resultados de la predicción son:')
+            st.write(predicted_values)
+
+def make_prediction(data):
+    # Aquí va tu lógica para realizar la predicción
+    # Esto puede variar según el modelo que estés utilizando
     
-    # Inicializamos el contador en 0
-    contador = st.number_input('Contador', value=0)
-    
-    # Botón para incrementar el contador
-    if st.button('Incrementar'):
-        contador += 1
-    
-    # Botón para reiniciar el contador
-    if st.button('Reiniciar'):
-        contador = 0
-    
-    # Mostrar el contador actualizado
-    st.write('El contador es:', contador)
+    # Ejemplo con regresión lineal
+    model = loaded_model = pickle.load(open('iris_model.pkl', "rb"))
+    predictions = model.predict(data)  # Realiza la predicción
+
+    return predictions
 
 if __name__ == "__main__":
     main()
