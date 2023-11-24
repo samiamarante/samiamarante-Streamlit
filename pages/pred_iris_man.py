9a311f78e9b2582edd3a809e14361ddb93454a7e
ffer <ffer200395@gmail.com>
2023-11-24 17:55:56 +0100

    Pred con Streamlit

--- pages/pred_iris_man.py
+++ pages/pred_iris_man.py
@@ -1,15 +1,31 @@
 import streamlit as st
-from utils import predict_flores
 import pandas as pd
+from utils import predict_flores
 
+# Título de la aplicación
+st.title('Predicción de flores')
+
+# Texto introductorio
 st.write('**Ingresa los datos manualmente para realizar la predicción de la flor:**')
 
+# Diccionario para almacenar los datos de entrada
 input_data = {}
+
+# Lista de columnas para las características de la flor
 columns = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
 
+# Bucle para recorrer las columnas y obtener los datos de entrada
 for col in columns:
+    # Widget de entrada numérica para cada característica
     input_data[col] = st.number_input(col, value=0.0)
 
+# Botón para realizar la predicción
 if st.button('Realizar Predicción'):
-    predicted_value = predict_flores(pd.DataFrame(input_data))
+    # Convertir el diccionario de entrada a un DataFrame de una sola fila
+    input_df = pd.DataFrame([input_data])
+
+    # Realizar la predicción utilizando la función predict_flores
+    predicted_value = predict_flores(input_df)
+
+    # Mostrar el resultado de la predicción
     st.write('El resultado de la predicción es:', predicted_value)
--- utils.py
+++ utils.py
@@ -1,13 +1,16 @@
 import pickle
 import streamlit as st
 
+def predict_flores(data):
+    # Cargar el modelo previamente entrenado para predecir el tipo de flor
+    model = pickle.load(open('models/iris_model.pkl', "rb"))
+    # Realizar la predicción con los datos proporcionados
+    predictions = model.predict(data) 
+    return predictions
+
 def check_client_id(client_id):
+    # Simulación
     # Cargar credenciales para la BBDD de la empresa y consultar si el identificador del cliente está activo 
     api_key = st.secrets["DB_USERNAME"]
     ls_ids = [123,12345,12345678]
     return True if client_id in ls_ids else False
-
-def predict_flores(data):
-    model = pickle.load(open('iris_model.pkl', "rb"))
-    predictions = model.predict(data) 
-    return predictions
