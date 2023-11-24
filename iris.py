fbaba24a6e9ebf5a1771f6816af63e7056960c8c
ffer <ffer200395@gmail.com>
2023-11-24 15:49:39 +0100

    Pred con Streamlit

--- iris.py
+++ iris.py
@@ -1,12 +1,11 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 import pickle
+
 iris = datasets.load_iris(as_frame=True)
 X = iris['data']
 y = iris['target']
-print(y)
-log_reg = LogisticRegression()
-log_reg.fit(X,y)
+log_reg = LogisticRegression().fit(X,y)
 
-with open('iris_model.pkl', 'wb') as f:  # open a text file
-    pickle.dump(log_reg, f) # serialize the list
+with open('iris_model.pkl', 'wb') as f: 
+    pickle.dump(log_reg, f)
--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -1,69 +1,45 @@
 import streamlit as st
 import pandas as pd
-import pickle
-from sklearn.linear_model import LogisticRegression
-
+from utils import *
 # Local: streamlit run streamlit_tutorial.py
 # Streamlit Sharing 
 # render, heroku, AWS EC2
 # ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)','petal width (cm)']
 def main():
-    option = st.radio('Selecciona una opción:', ('Ingresar Datos', 'Usar Datos del CSV'))
+    st.title('Bienvendo al portal predictivo de la empresa XYZ')
+    st.write('**Por favor seleccione qué sistema predictivo desea utilizar hoy:**')
+    sist_pred = st.radio('Selecciona una opción:', ('Quiero predecir flores', 'Quiero predecir imágenes'))
 
-    if option == 'Ingresar Datos':
-        st.write('**Ingresa los datos para realizar la predicción:**')
-        
-        # Aquí puedes solicitar los datos para la predicción
-        # Por ejemplo, si estás prediciendo con un modelo de regresión lineal:
-        input_data = {}
-        input_data['sepal length (cm)'] = [st.number_input('sepal length (cm)', value=0.0)]
-        input_data['sepal width (cm)'] = [st.number_input('sepal width (cm)', value=0.0)]
-        input_data['petal length (cm)'] = [st.number_input('petal length (cm)', value=0.0)]
-        input_data['petal width (cm)'] = [st.number_input('petal width (cm)', value=0.0)]
-        # Continúa con todas las características necesarias para la predicción
-        
-        if st.button('Realizar Predicción'):
-            # Realiza la predicción con los datos ingresados
-            predicted_value = make_prediction(pd.DataFrame(input_data))
-            st.write('El resultado de la predicción es:', predicted_value)
+    if sist_pred=='Quiero predecir flores':
+        option = st.radio('¿Cómo desea realizar la predicción?:', ('Ingresando datos manualmente', 'Subiendo un archivo CSV'))
 
-    elif option == 'Usar Datos del CSV':
-        st.title('Predicción con Archivo CSV')
-        # Widget para subir archivos CSV
-        uploaded_file = st.file_uploader("Selecciona un archivo CSV", type=['csv'])
-        if uploaded_file is not None:              
-            # Lee el archivo CSV como un DataFrame
-            df = pd.read_csv(uploaded_file)
-            @st.cache
-            def cache_uploaded_file(content):
-                return content
-            cached_content = cache_uploaded_file(df)
-            st.write('**Vista Previa del DataFrame:**')
-            # Muestra el DataFrame
-            st.write(df)
-            st.subheader('Realizar Predicción')
-            st.write('**Selecciona las columnas para la predicción:**')
-            # Aquí el usuario elige qué columnas usar para la predicción
-            # Por ejemplo, si se tienen columnas 'Feature_1', 'Feature_2', 'Target':
-            feature_cols = st.multiselect('Selecciona las características', cached_content.columns)
-        if st.button('Realizar Predicción con CSV'):
-            # Realiza la predicción con los datos del CSV seleccionados
-            predicted_values = make_prediction(cached_content[feature_cols])
-            st.write('Los resultados de la predicción son:')
-            st.write(predicted_values)
+        if option == 'Ingresando datos manualmente':
+            st.write('**Ingresa los datos para realizar la predicción de la flor:**')
+            input_data = {}
+            input_data['sepal length (cm)'] = [st.number_input('sepal length (cm)', value=0.0)]
+            input_data['sepal width (cm)'] = [st.number_input('sepal width (cm)', value=0.0)]
+            input_data['petal length (cm)'] = [st.number_input('petal length (cm)', value=0.0)]
+            input_data['petal width (cm)'] = [st.number_input('petal width (cm)', value=0.0)]            
+            if st.button('Realizar Predicción'):
+                predicted_value = predict_flores(pd.DataFrame(input_data))
+                st.write('El resultado de la predicción es:', predicted_value)
 
-def make_prediction(data):
-    # Aquí va tu lógica para realizar la predicción
-    # Esto puede variar según el modelo que estés utilizando
-    
-    # Ejemplo con regresión lineal
-    try:
-        model = pickle.load(open('iris_model.pkl', "rb"))
-    except Exception as e:
-        st.write(e)
-    else:
-        predictions = model.predict(data)  # Realiza la predicción
-        return predictions
+        elif option == 'Subiendo un archivo CSV':
+            st.title('Predicción de flores en archivo CSV')
+            uploaded_file = st.file_uploader("Selecciona un archivo CSV", type=['csv'])
+            if uploaded_file is not None:              
+                df = pd.read_csv(uploaded_file)
+                st.write('**Vista Previa del DataFrame:**')
+                st.write(df)
+                st.subheader('Realizar Predicción')
+                st.write('**Selecciona las columnas para la predicción:**')
+                feature_cols = st.multiselect('Selecciona las características', df.columns)
+                if st.button('Realizar Predicción con CSV'):
+                    predicted_values = predict_flores(df[feature_cols])
+                    st.write('Los resultados de la predicción son:')
+                    st.write(predicted_values)
+    elif sist_pred=='Quiero predecir imágenes':
+        st.write('En construcción')
 
 if __name__ == "__main__":
     main()
