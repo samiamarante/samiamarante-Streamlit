03e8fb51e71b92895326862a701e0a0d687eb2d3
ffer <ffer200395@gmail.com>
2024-04-15 16:17:19 +0200

    actualización abril 2024

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -1,31 +1,30 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+def main():
+    st.title('Bienvenido al portal predictivo de la empresa XYZ')
+    st.write('**Por favor seleccione el servicio predictivo que desea utilizar**')
+    
+    opcion = st.radio('Seleccione el servicio:', 
+                      ('Predicción del tipo de flor (con CSV)', 'Predicción del tipo de flor (manualmente)', 'Predicción de imagen'), 
+                      index=0, 
+                      key='option')
+    
+    if st.button('Empezar!'):
+        route_prediction(opcion)
+
+def route_prediction(opcion):
+    if opcion == 'Predicción del tipo de flor (con CSV)':
+        switch_page("pred_iris_csv")
+    elif opcion == 'Predicción del tipo de flor (manualmente)':
+        switch_page("pred_iris_man")
+    elif opcion == 'Predicción de imagen':
+        switch_page("pred_imagen")
+
+if __name__ == "__main__":
+    main()
+
 # https://docs.streamlit.io/library/get-started/multipage-apps
 # Local: streamlit run streamlit_tutorial.py
 # Streamlit Sharing 
 # render, heroku, AWS EC2
-
-def main():
-    # Bienvenida y selección del servicio
-    st.title('Bienvenido al portal predictivo de la empresa XYZ')
-    st.write('**Por favor seleccione el servicio predictivo que desea utilizar**')
-
-    # Selección del servicio entre dos opciones
-    opcion = st.radio('Seleccione el servicio:', ('Predicción del tipo de flor', 'Predicción de imagen'), index=None)
-
-    # Redirección a la página del servicio seleccionado
-    if opcion == 'Predicción del tipo de flor':
-        way_to_pred = st.radio('¿Cómo desea realizar la predicción de la flor?', ('Ingresando datos manualmente', 'Subiendo un archivo CSV'), index=None)
-        if way_to_pred == 'Ingresando datos manualmente':
-            # Redirigir a la página de predicción de iris con datos manuales
-            switch_page("pred_iris_man")
-        elif way_to_pred == 'Subiendo un archivo CSV':
-            # Redirigir a la página de predicción de iris con archivo CSV
-            switch_page("pred_iris_csv")
-    elif opcion == 'Predicción de imagen':
-        # Redirigir a la página de predicción de imagen
-        switch_page("pred_imagen")
-    
-if __name__ == "__main__":
-    main()
